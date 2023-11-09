3d9d403c63c45a4c8a56dc92b629c93925cb325e
Gabri <gabri88@mail.ru>
2023-11-09 15:50:17 +0400

    Создание классов

---
+++ sandbox/src/main/java/TEst.java
@@ -0,0 +1,10 @@
+public class TEst {
+    public static void main (String[] args) {
+        System.out.print(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.print(2/2);
+        System.out.println(2+2+5);
+        // методы
+    }
+}
